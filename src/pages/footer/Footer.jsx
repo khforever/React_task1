a23417e96adba062a25aac51c88e46bd9f9c77f7
khforever <59407743+khforever@users.noreply.github.com>
2025-06-13 04:55:18 +0300

    add footer

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -8,9 +8,41 @@ export default function Footer() {
   return (
 
 
+<div className="footer">
+  <samp> 
+ Show your creativity with Blayden - unique portfolio 
+ <br/>
+ template - and get noticed today! 
 
- ""
+ </samp>
+<br/>
+<div className="purchase-container">
+<button className="purchase">Purchase Now</button>
+</div>
 
+
+
+
+
+
+
+
+<div className="decoration left">
+        <div className="rock left-rock"></div>
+        
+      </div>
+    
+      <div className="decoration right">
+        <div className="rock right-rock"></div>
+     
+      </div>
+
+ 
+
+
+
+
+</div>
 
     
   )
